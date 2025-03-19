8bd25ac994266d5c5d811fc82374935b82604f91
JadaRose237 <rosejada420@gmail.com>
2025-03-19 18:05:48 +0000

    save + edit functions

--- src/app/members(dashboard)/script.js
+++ src/app/members(dashboard)/script.js
@@ -27,15 +27,40 @@ document.addEventListener("DOMContentLoaded", function (){
     });
 
     // save and edit
+    function addEditSaveFunctionality(row) {
+        row.querySelector(".edit").addEventListener("click", function () {
+            let editables = row.querySelectorAll(".editable");
 
+            editables.forEach(cell => {
+                let input = document.createElement("input");
+                input.value = cell.textContent;
+                cell.textContent = "";
+                cell.appendChild(input);
+            });
+            this.style.display= "none";
+            row.querySelector(".save").style.display = "inline";
+        });
+
+        row.querySelector(".save").addEventListener("click", function () {
+            let confirmDelete = confirm("Are you sure that you want to permenantly delete this member?");
+            if (confirmDelete) {
+                row.remove();
+            }
+        });
+
+    }
+
+    document.querySelectorAll(".edit").forEach(button => {
+        let row = button.closest("tr");
+        addEditSaveFunctionality(row);
+    });
 
 
 
 
 
     // add members
-
-
+    
 
 
 
